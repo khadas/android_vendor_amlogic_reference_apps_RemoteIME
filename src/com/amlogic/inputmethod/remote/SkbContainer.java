c445c943376b46fb334556e222974d50298cfa3f
lyra.lee <lyra.lee@amlogic.com>
2011-08-08 18:36:39 +0800

    fix a bug:cannot move focus to some buttons

--- src/com/amlogic/inputmethod/remote/SkbContainer.java
+++ src/com/amlogic/inputmethod/remote/SkbContainer.java
@@ -665,7 +665,7 @@ public class SkbContainer extends RelativeLayout implements OnTouchListener {
             return mSoftKeyFocus;
         }
         else if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
-            if(mSoftKeyFocus==null)             
+   /*         if(mSoftKeyFocus==null)             
                 mSoftKeyFocus=skb.getKey(0,-1);     
             else{
                 int row = mSoftKeyFocus.mRow;
@@ -684,10 +684,18 @@ public class SkbContainer extends RelativeLayout implements OnTouchListener {
                         row = i;                    
                 }           
                 mSoftKeyFocus = skb.getKey(row,loc);
+            } */
+            if(mSoftKeyFocus==null)             
+                mSoftKeyFocus=skb.getKey(0,0);      
+            else
+            {
+                int x = mSoftKeyFocus.mLeft-skb.getKeyXMargin()-10;
+                int y = mSoftKeyFocus.mTop+mSoftKeyFocus.height()/2;
+                mSoftKeyFocus = skb.mapToKey(x,y);
             }
         }
         else if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT) {
-            if(mSoftKeyFocus==null)             
+   /*         if(mSoftKeyFocus==null)             
                 mSoftKeyFocus=skb.getKey(0,0);      
             else{
                 int row = mSoftKeyFocus.mRow;
@@ -705,6 +713,15 @@ public class SkbContainer extends RelativeLayout implements OnTouchListener {
                     else
                         mSoftKeyFocus = skb.getKey(row, loc);
                 }                           
+            }*/
+            
+            if(mSoftKeyFocus==null)             
+                mSoftKeyFocus=skb.getKey(0,0);      
+            else
+            {
+                int x = mSoftKeyFocus.mRight+skb.getKeyXMargin();
+                int y = mSoftKeyFocus.mTop+mSoftKeyFocus.height()/2;
+                mSoftKeyFocus = skb.mapToKey(x,y);
             }
                 
         }
